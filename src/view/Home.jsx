24affec0491ee3222fba8e9dedacf8f302e39324
ramirols <ramirols.dev>
2024-12-07 15:29:37 -0500

    agregacion de 1 seccion

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
             <VideoSection id="section2" videoSrc="/assets/videos/video2.mp4" title="Sección 2" />
             <VideoSection id="section3" videoSrc="/assets/videos/video3.mp4" title="Sección 3" />
             <VideoSection id="section4" videoSrc="/assets/videos/video4.mp4" title="Sección 4" />
+            <VideoSection id="section5" videoSrc="/assets/videos/video5.mp4" title="Sección 5" />
         </div>
     );
 }
